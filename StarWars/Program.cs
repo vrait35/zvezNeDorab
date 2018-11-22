0b09d2a65e57c12ba1cacf1f6fdc065bd1671ddc
vrait35 <vrait35@mail.ru>
2018-11-22 13:55:19 +0600

    asd

---
+++ StarWars/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using Newtonsoft.Json;
+using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StarWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("введите id  планеты");
+            int idPlanet = int.Parse(Console.ReadLine());
+            StarWarsResponse starWarsResponse = new StarWarsResponse();
+            List<People> list = new List<People>();
+            starWarsResponse.Planet(idPlanet, list);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
